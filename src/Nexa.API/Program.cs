93ea43e9678ad974045777e900059260828930e7
Rafael Honorio <133076581+honorioRafael@users.noreply.github.com>
2026-03-14 20:07:08 -0300

    Adicionado para o swagger utilizar autenticação

--- src/Nexa.API/Program.cs
+++ src/Nexa.API/Program.cs
@@ -1,27 +1,41 @@
+using Microsoft.OpenApi;
 using Nexa.API.Extensions;
 using Nexa.Application;
 using Nexa.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllers();
 
-// Hexagonal Architecture - layer registrations
 builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
 
-// Project specific dependencies
 builder.Services.AddProjectDependencies();
 
-// Swagger
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+   
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "Insira o token JWT desta maneira: Bearer {seu token}",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    c.AddSecurityRequirement(document => new OpenApiSecurityRequirement
+    {
+       
+        [new OpenApiSecuritySchemeReference("Bearer", document)] = []
+    });
+});
+
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
